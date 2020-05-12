0fd4c43fce668edbe72f0d3c28c14d125a593589
paulemxx <51763335+paulemxx@users.noreply.github.com>
2020-05-12 20:46:01 +0000

    admin blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,141 @@
 from django.contrib import admin
+from . import models
+from django.utils.safestring import mark_safe
+
 
 # Register your models here.
+
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+        'status',
+        'date_add',
+        'date_update'
+    )
+    list_filter = (
+        'status',
+        'date_add',
+        'date_update',
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_pages = 50
+    date_hierarchy = 'date_add'
+  
+
+    fieldsets = [
+        ('Info ', {'fields': ['titre', ]}),
+        ('Status et Activations', {'fields': ['status', ]}),
+    ]
+
+   
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+        'status',
+        'date_add',
+        'date_upd'
+    )
+    list_filter = (
+        'status',
+        'date_add',
+        'date_upd',
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_pages = 50
+    date_hierarchy = 'date_add'
+  
+
+    fieldsets = [
+        ('Info ', {'fields': ['titre', ]}),
+        ('Status et Activations', {'fields': ['status', ]}),
+    ]
+
+
+
+
+class CommentaireAdmin(admin.ModelAdmin):
+    list_display = (
+        'article',
+        'message',
+        'nom',
+        'email',
+        'prenom',
+        'status',
+        'date_add',
+        'date_update' 
+    )
+
+    list_filter = (
+        'status',
+        'article',
+        'date_add',
+        'date_update'
+    )
+    search_fields = (
+        'message',
+    )
+    fieldsets = [
+        ('Info ', {'fields': [
+            
+            'article',
+            'message',
+            'nom',
+            'email',
+            'prenom',
+        ]
+        }),
+        ('Status et Activations', {'fields': ['status', ]}),
+    ]
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+        'affiche_image',
+        'status',
+        'date_add',
+        'date_update'
+    )
+    list_filter = (
+        'categorie',
+        'status',
+        'tags'
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_pages = 50
+    date_hierarchy = 'date_add'
+    readonly_fields = ['affiche_image']
+
+    fieldsets = [
+        ('Info ', {'fields': [
+            'titre',
+            'categorie',
+            'tags',
+            'contenu'
+        ]
+        }),
+        ('Image', {'fields': ['cover', 'affiche_image']}),
+        ('Status et Activations', {'fields': ['status', ]}),
+    ]
+
+    def affiche_image(self, obj):
+        return mark_safe('<img src="{url}" width="100px" height="50px" />'.format(url=obj.cover.url))
+
+
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.Article, ArticleAdmin)
+_register(models.Commentaire, CommentaireAdmin)
+_register(models.Categorie, CategorieAdmin)
+_register(models.Tag, TagAdmin)
