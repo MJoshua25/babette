00d0764aa7ade0598653ef82872217999488846a
Yao Josué <jyao2000@yahoo.fr>
2020-05-12 02:29:28 +0200

    template blog

--- projet/urls.py
+++ projet/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('', include('restaurant.urls')),
     path('shop', include('shop.urls')),
+    path('blog', include('blog.urls')),
     path('admin/filebrowser/', site.urls),
 ]
 
