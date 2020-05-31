f3c7495380e65f3110d0271e4a56f3fa30d952db
sfkd29 <58959324+sfkd29@users.noreply.github.com>
2020-05-31 19:23:06 +0200

    gbg

--- siteConfig/datamanager.py
+++ siteConfig/datamanager.py
@@ -1,5 +1,14 @@
 from django.http.request import HttpRequest
 from . import models
+from shop import models as shop_models
+
+
+def getCard(request: HttpRequest):
+    try:
+        card = shop_models.Commande.objects.get(id=request.session['card'])
+    except:
+        card = None
+    return card
 
 
 def getConfig(request: HttpRequest) -> dict:
@@ -9,7 +18,7 @@ def getConfig(request: HttpRequest) -> dict:
         "footers": models.Footer.objects.filter(status=True),
         "sponsors": models.Sponsor.objects.filter(status=True),
         "mainevents": models.Mainevent.objects.filter(status=True),
-        
+        "card": getCard(request)
     }
     return data
 
